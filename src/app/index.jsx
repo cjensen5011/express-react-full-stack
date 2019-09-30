8a7383d76c2de147b7d5e230143fe8985c2b8fa0
Christopher Jensen <christopher.jensen@daugherty.com>
2019-09-30 13:40:29 -0500

    initial commit

---
+++ src/app/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from './components/Main';
+import { store } from './store';
+
+ReactDOM.render(<Main />, document.getElementById('app'));
